d8f0da13060114bcf1d1db50d13af3d316514da2
TuDangVan <tudv@newwave.vn>
2023-06-29 16:23:57 +0700

    add base

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileFieldsInterceptor, FileInterceptor } from '@nestjs/platform-express';
+import { UploadRequestDto } from './common/dtos/upload-request.dto';
+import { ApiConsumes } from "@nestjs/swagger";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        {
+          name: 'file',
+          maxCount: 10,
+        },
+      ],
+    ),
+  )
+  uploadFile(@Body() body: UploadRequestDto, @UploadedFiles() files: Express.Multer.File) {
+    try {
+      this.appService.tranformFile(files);
+    } catch (error) {
+      return error;
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@nestjs/common';
-
+const Transform = require("stream").Transform;
+const zlib = require('zlib');
+const fs = require('fs');
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  // processing tranfrom stream
+  tranformFile(files: any) {
+    try {
+      if (!files) return;
+      console.log('tranformFile file === ', files?.file);
+      //
+      //	Create a new file which will contain the compressed data
+      //
+      const to_compressed_file = fs.createWriteStream(`${__dirname}/file.gz`, {
+        flags: 'w',
+        defaultEncoding: 'ascii',
+        fd: null,
+        mode: 0o666,
+        autoClose: true
+      });
+      console.log(99999999999999999);
+
+      //
+      //	Compressor
+      //
+      const gz = zlib.createGzip();
+
+      //
+      //	Pipe
+      //
+      // get stream data from file.txt pass it to stream.Transform Gzip then pass stream to file gz
+      files?.file?.[0]?.buffer
+        .pipe(gz)
+        .pipe(to_compressed_file);
+      console.log('111111111111111 files?.file?.[0]?.buffer ==== ', files?.file?.[0]?.buffer);
+    } catch (error) {
+      throw error
+    }
   }
 }
