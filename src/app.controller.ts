facadfb50272c3d01cf95d4d8e5fb78c31da6024
TuDangVan <tudv@newwave.vn>
2023-06-30 08:56:08 +0700

    add base nestjs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,30 +1,12 @@
-import { Body, Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { Get, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { FileFieldsInterceptor, FileInterceptor } from '@nestjs/platform-express';
-import { UploadRequestDto } from './common/dtos/upload-request.dto';
-import { ApiConsumes } from "@nestjs/swagger";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Post()
-  @ApiConsumes('multipart/form-data')
-  @UseInterceptors(
-    FileFieldsInterceptor(
-      [
-        {
-          name: 'file',
-          maxCount: 10,
-        },
-      ],
-    ),
-  )
-  uploadFile(@Body() body: UploadRequestDto, @UploadedFiles() files: Express.Multer.File) {
-    try {
-      this.appService.tranformFile(files);
-    } catch (error) {
-      return error;
-    }
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
